3280d4ca0e8fbfcb5ea3aef49a1e1ff90988e273
cambrown101 <cameron-brown@live.com>
2021-02-08 14:09:07 -0800

    Add React Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+      <Switch>
+        <Route path="home" component={Home} />
+      </Switch>
+       <Nav></Nav>
+    </Router>
     <div className="App">
       <h1>Cam Brown</h1>
     </div>
