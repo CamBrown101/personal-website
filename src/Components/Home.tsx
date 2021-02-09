519b733c42880734f018b50af5739a019344c86d
cambrown101 <cameron-brown@live.com>
2021-02-08 21:45:30 -0800

    Add some styles to home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,7 +4,14 @@ import './Home.scss';
 export const Home = () => {
   return (
     <div className="home-container">
-      <h1>Home</h1>
+      <article className="about-me-container">
+        <h3>About Me</h3>
+        <p>Some stuff about me</p>
+      </article>
+      <article className="career-container">
+        <h3>Career</h3>
+        <p>Some stuff about my career</p>
+      </article>
     </div>
   );
 };
