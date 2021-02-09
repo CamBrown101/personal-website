c84173426dcf28b487efe99cce1d11d62dee6135
cambrown101 <cameron-brown@live.com>
2021-02-08 21:36:07 -0800

    Add Projects Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Components/Home';
 import { About } from './Components/About';
 import { Contact } from './Components/Contact';
+import { Projects } from './Components/Projects';
 import { Nav } from './Components/Nav';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
+        <Route exact path="/projects" component={Projects} />
       </Switch>
       <Nav />
     </Router>
