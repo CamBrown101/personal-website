1c4a8857ce3fadb1f42fcbe6666e401b1dd70f9c
cambrown101 <cameron-brown@live.com>
2021-02-08 21:05:33 -0800

    Add contact and About buttons

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -6,7 +6,15 @@ export const Nav = () => {
   return (
     <nav id="nav-bar">
       <nav id="nav-menu">
-        <NavLink className="nav-menu-item" id="nav-menu-home" to="/home" />
+        <NavLink className="nav-menu-item" id="nav-menu-home" to="/">
+          Home
+        </NavLink>
+        <NavLink className="nav-menu-item" id="nav-menu-about" to="/">
+          About
+        </NavLink>
+        <NavLink className="nav-menu-item" id="nav-menu-contact" to="/">
+          Contact
+        </NavLink>
       </nav>
     </nav>
   );
