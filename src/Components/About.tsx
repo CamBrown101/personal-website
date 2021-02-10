8aedb4be75d8b9e4de86877252cb3ec1b433bb9c
cambrown101 <cameron-brown@live.com>
2021-02-09 17:52:54 -0800

    Add content

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import './About.scss';
 
 export const About = () => {
   return (
-    <div>
-      <h1>Experience</h1>
-      <p>Work History</p>
+    <div className="about-container">
+      <h1>Cameron Brown</h1>
+      <h2>Web Developer</h2>
+      <p>
+        I am a Full Stack Web Developer working with HTML, CSS, Javascript,
+        React, Node and SQL. Prior to becoming a developer, my educational
+        background was 3D animation, and I have over 15 years of experience in
+        hospitality. I discovered a love for coding during my days as a Bar
+        Manager doing inventory using spreadsheets and diving into more complex
+        ways I could make the process easier. I worked as an intern Junior Web
+        Developer prior to attending a full-time bootcamp at Lighthouse Labs.
+        This was a great experience and allowed me to get a feel for what it is
+        like to work in a web development environment.
+      </p>
     </div>
   );
 };
