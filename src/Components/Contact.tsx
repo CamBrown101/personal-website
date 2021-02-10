19da363eb86a48297091c2be7eb2e87105260283
cambrown101 <cameron-brown@live.com>
2021-02-09 17:44:43 -0800

    Edit contact types

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,7 +5,19 @@ export const Contact = () => {
   return (
     <div className="contact-container">
       <h1>Contact</h1>
-      <i className="fab fa-linkedin linkedin-icon"></i>
+      <h4>Connect With Me</h4>
+      <div className="flex">
+        <i className="fas fa-envelope-square mail-icon icon"></i>
+        <h3>cameron-brown@live.com</h3>
+      </div>
+      <div className="flex">
+        <i className="fab fa-linkedin linkedin-icon icon"></i>
+        <h3>cameron-brown-101/</h3>
+      </div>
+      <div className="flex">
+        <i className="fab fa-github-square github-icon icon"></i>
+        <h3>CamBrown101</h3>
+      </div>
     </div>
   );
 };
