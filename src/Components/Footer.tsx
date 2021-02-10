d5c1014d59bdc11d3ee15752d2f95bd3183eebb8
cambrown101 <cameron-brown@live.com>
2021-02-09 18:11:34 -0800

    Add footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Footer.scss';
 
 export const Footer = () => {
+  const todaysYear = new Date().getFullYear();
   return (
-    <footer className="footer">
-      <p>Footer Stuff</p>
+    <footer className="footer-container">
+      <p>
+        Copyright {todaysYear} Desgin and Developed By Cameron Brown. All Rights
+        Reserved.
+      </p>
     </footer>
   );
 };
