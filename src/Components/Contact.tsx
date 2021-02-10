b6b538bcf647d0cd4748cdebddfcc995b63c68d1
cambrown101 <cameron-brown@live.com>
2021-02-09 17:52:46 -0800

    Style content

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -4,15 +4,14 @@ import './Contact.scss';
 export const Contact = () => {
   return (
     <div className="contact-container">
-      <h1>Contact</h1>
-      <h4>Connect With Me</h4>
+      <h1>Contact Me</h1>
       <div className="flex">
         <i className="fas fa-envelope-square mail-icon icon"></i>
         <h3>cameron-brown@live.com</h3>
       </div>
       <div className="flex">
         <i className="fab fa-linkedin linkedin-icon icon"></i>
-        <h3>cameron-brown-101/</h3>
+        <h3>cameron-brown-101</h3>
       </div>
       <div className="flex">
         <i className="fab fa-github-square github-icon icon"></i>
