52214d0304113ec18cc093dfb0518e32e5c1fd65
cambrown101 <cameron-brown@live.com>
2021-02-09 18:00:26 -0800

    Add about content

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -4,9 +4,9 @@ import './About.scss';
 export const About = () => {
   return (
     <div className="about-container">
-      <h1>Cameron Brown</h1>
-      <h2>Web Developer</h2>
-      <p>
+      <h1 className="name">Cameron Brown</h1>
+      <h2 className="title">Web Developer</h2>
+      <p className="about-me">
         I am a Full Stack Web Developer working with HTML, CSS, Javascript,
         React, Node and SQL. Prior to becoming a developer, my educational
         background was 3D animation, and I have over 15 years of experience in
