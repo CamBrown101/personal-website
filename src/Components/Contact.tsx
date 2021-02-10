1c2b11dbff061de4e17d57f55bf27388765db38b
cambrown101 <cameron-brown@live.com>
2021-02-09 17:25:59 -0800

    Fix icons

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Contact.scss';
 
 export const Contact = () => {
   return (
-    <div>
+    <div className="contact-container">
       <h1>Contact</h1>
+      <i className="fab fa-linkedin linkedin-icon"></i>
     </div>
   );
 };
