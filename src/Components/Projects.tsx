64626d3380563d04f3f6ed92954687a1621e78d2
cambrown101 <cameron-brown@live.com>
2021-02-11 14:26:28 -0800

    Block out projects

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -21,6 +21,54 @@ export const Projects = () => {
           React, React-Router, SASS, HTML, Node, Express, Node-mailer, Canvas.js
         </p>
       </article>
+      <article className="project">
+        <img className="screenshot" alt="This is a project" src={ScreenShot1} />
+        <h4 className="project-title">Buy Food, Sell Food</h4>
+        <p className="project-description">
+          A point of sales system for a restaurant. Including analytics,
+          employee clock in, stripe integration, the ability to email bills,
+          sort bills by seat and a separate screen for the kitchen and bar for
+          items that need to be prepared. We also a sort by categories for menu
+          items as well as a search bar that you can search by item name or
+          category.
+        </p>
+        <h3 className="tech-stack-header">Tech Stack</h3>
+        <p className="tech-stack">
+          React, React-Router, SASS, HTML, Node, Express, Node-mailer, Canvas.js
+        </p>
+      </article>
+      <article className="project">
+        <img className="screenshot" alt="This is a project" src={ScreenShot1} />
+        <h4 className="project-title">Buy Food, Sell Food</h4>
+        <p className="project-description">
+          A point of sales system for a restaurant. Including analytics,
+          employee clock in, stripe integration, the ability to email bills,
+          sort bills by seat and a separate screen for the kitchen and bar for
+          items that need to be prepared. We also a sort by categories for menu
+          items as well as a search bar that you can search by item name or
+          category.
+        </p>
+        <h3 className="tech-stack-header">Tech Stack</h3>
+        <p className="tech-stack">
+          React, React-Router, SASS, HTML, Node, Express, Node-mailer, Canvas.js
+        </p>
+      </article>
+      <article className="project">
+        <img className="screenshot" alt="This is a project" src={ScreenShot1} />
+        <h4 className="project-title">Buy Food, Sell Food</h4>
+        <p className="project-description">
+          A point of sales system for a restaurant. Including analytics,
+          employee clock in, stripe integration, the ability to email bills,
+          sort bills by seat and a separate screen for the kitchen and bar for
+          items that need to be prepared. We also a sort by categories for menu
+          items as well as a search bar that you can search by item name or
+          category.
+        </p>
+        <h3 className="tech-stack-header">Tech Stack</h3>
+        <p className="tech-stack">
+          React, React-Router, SASS, HTML, Node, Express, Node-mailer, Canvas.js
+        </p>
+      </article>
     </div>
   );
 };
