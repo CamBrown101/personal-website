0144aff9652b00e42897371b6061310d1df4d1a9
cambrown101 <cameron-brown@live.com>
2021-02-10 18:49:04 -0800

    Create project layout

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Projects.scss';
-import ScreenShot from '../images/ScreenShot.png';
+import ScreenShot1 from '../images/ScreenShot.png';
 
 export const Projects = () => {
   return (
     <div className="projects-container">
-      <div className="project">
-        <img className="screenshot" src={ScreenShot} />
-      </div>
+      <article className="project">
+        <img className="screenshot" alt="This is a project" src={ScreenShot1} />
+        <h4>Buy Food, Sell Food</h4>
+        <p>
+          A point of sales system for a restaurant. Including analytics,
+          employee clock in, stripe integration, the ability to email bills,
+          sort bills by seat and a separate screen for the kitchen and bar for
+          items that need to be prepared. We also a sort by categories for menu
+          items as well as a search bar that you can search by item name or
+          category.
+        </p>
+        <h3 className="tech-stack-header">Tech Stack</h3>
+        <p className="tech-stack">
+          React, React-Router, SASS, HTML, Node, Express, Node-mailer, Canvas.js
+        </p>
+      </article>
     </div>
   );
 };
