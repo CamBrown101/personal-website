21a5cd9a01ec1727019cbd13f226da8ff04d149e
cambrown101 <cameron-brown@live.com>
2021-02-10 18:20:30 -0800

    Add hover on contacts

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,23 +5,21 @@ export const Contact = () => {
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <a href="mailto:cameron-brown@live.com?subject=Mail from Cameron Brown's Website">
-        <div className="flex">
-          <i className="fas fa-envelope-square mail-icon icon"></i>
-          <h3>cameron-brown@live.com</h3>
-        </div>
+      <a
+        className="flex contact"
+        href="mailto:cameron-brown@live.com?subject=Mail from Cameron Brown's Website">
+        <i className="fas fa-envelope-square mail-icon icon"></i>
+        <h3>Cameron-Brown@live.com</h3>
       </a>
-      <a href="https://www.linkedin.com/in/cameron-brown-101/">
-        <div className="flex">
-          <i className="fab fa-linkedin linkedin-icon icon"></i>
-          <h3>cameron-brown-101</h3>
-        </div>
+      <a
+        className="flex contact"
+        href="https://www.linkedin.com/in/cameron-brown-101/">
+        <i className="fab fa-linkedin linkedin-icon icon"></i>
+        <h3>Cameron-Brown-101</h3>
       </a>
-      <a href="https://github.com/CamBrown101">
-        <div className="flex">
-          <i className="fab fa-github-square github-icon icon"></i>
-          <h3>CamBrown101</h3>
-        </div>
+      <a className="flex contact" href="https://github.com/CamBrown101">
+        <i className="fab fa-github-square github-icon icon"></i>
+        <h3>CamBrown101</h3>
       </a>
     </div>
   );
