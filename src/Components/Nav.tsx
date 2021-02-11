17b60d808e0d4480575f40c1ef5e198dc67a8a98
cambrown101 <cameron-brown@live.com>
2021-02-11 15:34:59 -0800

    Space out Nav buttons

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -6,30 +6,32 @@ export const Nav = () => {
   return (
     <nav id="nav-bar">
       <nav id="nav-menu">
-        <NavLink exact to="/" className="nav-menu-item" id="nav-menu-home">
-          Home
-        </NavLink>
-        <NavLink
-          exact
-          to="/about"
-          className="nav-menu-item"
-          id="nav-menu-about">
-          About
-        </NavLink>
-        <NavLink
-          exact
-          to="/contact"
-          className="nav-menu-item"
-          id="nav-menu-contact">
-          Contact
-        </NavLink>
-        <NavLink
-          exact
-          to="/projects"
-          className="nav-menu-item"
-          id="nav-menu-projects">
-          Projects
-        </NavLink>
+        <div className="sections">
+          <NavLink exact to="/" className="nav-menu-item" id="nav-menu-home">
+            Home
+          </NavLink>
+          <NavLink
+            exact
+            to="/about"
+            className="nav-menu-item"
+            id="nav-menu-about">
+            About
+          </NavLink>
+          <NavLink
+            exact
+            to="/contact"
+            className="nav-menu-item"
+            id="nav-menu-contact">
+            Contact
+          </NavLink>
+          <NavLink
+            exact
+            to="/projects"
+            className="nav-menu-item"
+            id="nav-menu-projects">
+            Projects
+          </NavLink>
+        </div>
         <div className="nav-contacts">
           <a
             className="nav-contact"
