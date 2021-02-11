f6ac380f495451a074efd8f71807a80c0debdc7d
cambrown101 <cameron-brown@live.com>
2021-02-10 18:06:54 -0800

    Add links to contact types

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,18 +5,24 @@ export const Contact = () => {
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <div className="flex">
-        <i className="fas fa-envelope-square mail-icon icon"></i>
-        <h3>cameron-brown@live.com</h3>
-      </div>
-      <div className="flex">
-        <i className="fab fa-linkedin linkedin-icon icon"></i>
-        <h3>cameron-brown-101</h3>
-      </div>
-      <div className="flex">
-        <i className="fab fa-github-square github-icon icon"></i>
-        <h3>CamBrown101</h3>
-      </div>
+      <a href="mailto:cameron-brown@live.com?subject=Mail from Cameron Brown's Website">
+        <div className="flex">
+          <i className="fas fa-envelope-square mail-icon icon"></i>
+          <h3>cameron-brown@live.com</h3>
+        </div>
+      </a>
+      <a href="https://www.linkedin.com/in/cameron-brown-101/">
+        <div className="flex">
+          <i className="fab fa-linkedin linkedin-icon icon"></i>
+          <h3>cameron-brown-101</h3>
+        </div>
+      </a>
+      <a href="https://github.com/CamBrown101">
+        <div className="flex">
+          <i className="fab fa-github-square github-icon icon"></i>
+          <h3>CamBrown101</h3>
+        </div>
+      </a>
     </div>
   );
 };
