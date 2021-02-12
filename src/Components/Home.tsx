2069219afc141ae89b1207c5c1f7379fa4e22cbb
cambrown101 <cameron-brown@live.com>
2021-02-12 15:57:09 -0800

    Add Skils component

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { About } from './About';
 import { Contact } from './Contact';
 import { Projects } from './Projects';
+import { Skills } from './Skills';
 import './Home.scss';
 
 export const Home = () => {
   return (
     <div className="home-container">
       <About />
+      <Skills />
       <article className="career-container">
         <h3>Career</h3>
         <p>Some stuff about my career</p>
