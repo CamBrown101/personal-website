4caa3ea3d39ea7c74fda5fa6bbe82c944b49a0fd
cambrown101 <cameron-brown@live.com>
2021-02-11 16:05:30 -0800

    Add skills

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -5,6 +5,31 @@ export const Skills = () => {
   return (
     <div>
       <h3>Skills Section</h3>
+      <h4>Languages</h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>SASS</li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>SQL</li>
+        <li>C#</li>
+        <li>Ruby</li>
+      </ul>
+      <h4>Frameworks</h4>
+      <ul>
+        <li>React</li>
+        <li>React-Router</li>
+        <li>React-Native</li>
+        <li>Redux</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>Rails</li>
+        <li>jQuery</li>
+        <li>Bootstrap</li>
+        <li>.Net</li>
+      </ul>
+      <h4>Experience</h4>
     </div>
   );
 };
