687f463e89b691c061bb79d78a189812a20d2653
cambrown101 <cameron-brown@live.com>
2021-02-12 19:52:28 -0800

    Add hover effect to projects

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Project.scss';
 type Props = {
   title: string;
   image: string;
@@ -7,15 +7,22 @@ type Props = {
   techstack: string;
 };
 
-export const Project = ({ title, image, description, techstack }) => {
+export const Project = ({ title, image, description, techstack }: Props) => {
+  const [hover, setHover] = useState<boolean>(false);
+
   return (
-    <article className="project">
+    <article
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => {
+        setHover(false);
+      }}
+      className="project">
       <img className="screenshot" alt="This is a project" src={image} />
       <h4 className="project-title">{title}</h4>
       <p className="project-description">{description}</p>
       <h3 className="tech-stack-header">Tech Stack</h3>
       <p className="tech-stack">{techstack}</p>
-      <div className="project-buttons"></div>
+      <div className={hover ? 'project-buttons' : 'project-buttons hide'}></div>
     </article>
   );
 };
