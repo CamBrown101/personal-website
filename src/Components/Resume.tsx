373fe32e77783c19ff8945424dc73bf48637ebad
cambrown101 <cameron-brown@live.com>
2021-02-16 11:15:24 -0800

    Create resume component

---
+++ src/Components/Resume.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Resume.scss';
+
+export const Resume = () => {
+  return (
+    <div id="resume-container">
+      <a href="" className="resume-button">
+        To view my full resume click here
+      </a>
+    </div>
+  );
+};
