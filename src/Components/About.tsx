4db7685bd5fcce2025628031ec6ba2bdf0537837
cambrown101 <cameron-brown@live.com>
2021-02-16 17:31:08 -0800

    Cut down paragraph

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -10,12 +10,7 @@ export const About = () => {
         I am a Full Stack Web Developer working with HTML, CSS, Javascript,
         React, Node and SQL. Prior to becoming a developer, my educational
         background was 3D animation, and I have over 15 years of experience in
-        hospitality. I discovered a love for coding during my days as a Bar
-        Manager doing inventory using spreadsheets and diving into more complex
-        ways I could make the process easier. I worked as an intern Junior Web
-        Developer prior to attending a full-time bootcamp at Lighthouse Labs.
-        This was a great experience and allowed me to get a feel for what it is
-        like to work in a web development environment.
+        hospitality.
       </p>
     </div>
   );
